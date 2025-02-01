2d762602f9936c65b49ceaf0c3dd0daa206e53d4
tumansdev <momentum.ipmini@gmail.com>
2025-02-01 15:17:59 +0700

    add text first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,9 @@ export default function Home() {
           height={38}
           priority
         />
+
+        <h1>สวัสดีจ้า</h1>
+        <p>ยินดีต้อนรับเข้าสู่ Next.js</p>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
